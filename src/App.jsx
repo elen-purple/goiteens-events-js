f08f3fdeebb8bf433302fcec66f6fb938aafb033
elen-purple <anna.havrylova.2011@gmail.com>
2025-08-02 17:25:08 +0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { BiBeenHere } from "react-icons/bi";
 import { BiFace } from "react-icons/bi";
 import events from "./data/upcoming-events.json";
 import { PageBoard } from "./components/PageBoard/PageBoard.jsx";
+import { GlobalStyle } from "./GlobalStyles.js";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <PageBoard
         icons={[BiAlarm, BiCalendarAlt, BiBeenHere, BiFace]}
         events={events}
--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,3 +1,6 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
 * {
   margin: 0;
   padding: 0;
@@ -11,3 +14,4 @@ ul {
 body {
   font-family: "Trebuchet MS";
 }
+`;
